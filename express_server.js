db1eaeb4aae9a4dd3458646479758e423b8b3925
Mathew M <mathewmastromonaco@gmail.com>
2024-07-09 14:00:24 -0400

    bugfixed once more to properly show the longURL and added the _header file

--- express_server.js
+++ express_server.js
@@ -13,6 +13,16 @@ app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
@@ -24,3 +34,4 @@ app.get('/urls.json', (req, res) => {
 app.get('/hello', (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
