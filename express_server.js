b91e1bd8f304d0b7f86fdec1561f264d441f44ea
Mathew M <mathewmastromonaco@gmail.com>
2024-07-09 14:44:19 -0400

    added the functionality to accept submissions of new longURLS and created a function to generate a random string for the shortURL

--- express_server.js
+++ express_server.js
@@ -9,6 +9,13 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
 app.get('/', (req, res) => {
   res.send('Hello!');
 });
@@ -16,6 +23,10 @@ app.get('/', (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
